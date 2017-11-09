ad366a2101ebc2728ff29f6c8be76bb640b6b0fe
Alena Zubrevich <Alena_Zubrevich@epam.com>
2017-11-09 16:38:30 +0300

    drop unused files

--- src/main/java/com/epam/jira/TestManagementMojo.java
+++ src/main/java/com/epam/jira/TestManagementMojo.java
@@ -16,16 +16,23 @@ package com.epam.jira;
  * limitations under the License.
  */
 
+import org.apache.maven.artifact.Artifact;
 import org.apache.maven.model.Dependency;
 import org.apache.maven.model.Plugin;
+import org.apache.maven.model.PluginExecution;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.util.xml.Xpp3Dom;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Mojo(name = "test-management", defaultPhase = LifecyclePhase.COMPILE)
 public class TestManagementMojo extends AbstractMojo {
 
     @Parameter(property = "project", required = true, readonly = true)
@@ -35,31 +42,32 @@ public class TestManagementMojo extends AbstractMojo {
         boolean hasTestNG = containsDependencyById("testng");
         boolean hasJunit = containsDependencyById("junit");
 
-        Plugin surefirePlugin = lookupPlugin("org.apache.maven.plugins:maven-surefire-plugin");
+        Plugin surefirePlugin = project.getPlugin("org.apache.maven.plugins:maven-surefire-plugin");
 
-        //todo add if null ? project.addPlugin();
+        getLog().info("Surefire Plugin: " + surefirePlugin.getArtifactId() + " v." + surefirePlugin.getVersion());
 
         Object config = updateConfiguration(hasJunit, hasTestNG, surefirePlugin.getConfiguration());
         surefirePlugin.setConfiguration(config);
 
-    }
-
-    private Plugin lookupPlugin(String pluginName) {
-        List<Plugin> plugins = project.getBuildPlugins();
-
-        for (Plugin plugin : plugins)
-            if (pluginName.equalsIgnoreCase(plugin.getKey()))
-                return plugin;
-
-        return null;
+        List<PluginExecution> executions = surefirePlugin.getExecutions();
+        for (PluginExecution execution : executions) {
+            if (execution.getId().equals("default-test")) {
+                getLog().info("Setting DEFAULT-TEST");
+                config = updateConfiguration(hasJunit, hasTestNG, execution.getConfiguration());
+                execution.setConfiguration(config);
+                getLog().info("Configuration:" + config);
+                break;
+            }
+        }
     }
 
     private boolean containsDependencyById(String artifactId) {
         List<Dependency> dependencies = project.getDependencies();
 
-        for (Dependency dependency : dependencies)
+        for (Dependency dependency : dependencies) {
             if (dependency.getArtifactId().equalsIgnoreCase(artifactId))
                 return true;
+        }
 
         return false;
     }
@@ -68,16 +76,23 @@ public class TestManagementMojo extends AbstractMojo {
 
         if (!hasJunit && !hasTestNG) return null;
 
-        if (configuration == null)
+        if (configuration == null) {
+            getLog().info("Create new configuration for Surefire plugin");
             configuration = new Xpp3Dom("configuration");
+        } else {
+            getLog().info("Configuration found: " + configuration.toString());
+        }
 
         if (configuration instanceof Xpp3Dom) {
 
             Xpp3Dom xml = (Xpp3Dom) configuration;
             Xpp3Dom properties = xml.getChild("properties");
             if (properties == null) {
+                getLog().info("Create new properties for Surefire plugin");
                 properties = new Xpp3Dom("properties");
                 xml.addChild(properties);
+            } else {
+                getLog().info("Properties found: " + properties.toString());
             }
             Xpp3Dom[] propertyList = properties.getChildren("property");
 
@@ -86,8 +101,10 @@ public class TestManagementMojo extends AbstractMojo {
             for (Xpp3Dom property : propertyList) {
                 if (property.getChild("name").getValue().equalsIgnoreCase("listener")) {
                     listenerProperty = property;
+                    getLog().info("Listener found: " + property.getValue());
                     break;
                 }
+                getLog().info("Property found: " + property.getName() + " " + property.getValue());
             }
             if (listenerProperty == null) {
                 Xpp3Dom name = new Xpp3Dom("name");
@@ -99,13 +116,19 @@ public class TestManagementMojo extends AbstractMojo {
                 properties.addChild(listenerProperty);
             }
 
-            String newValue = listenerProperty.getChild("value").getValue()
-                    + (hasTestNG ? "," + com.epam.jira.testng.ExecutionListener.class.getName() : "")
-                    + (hasJunit ? "," + com.epam.jira.junit.ExecutionListener.class.getName() : "" );
-            listenerProperty.setValue(newValue);
-            //todo check if properties addChild is needed
+            List<String> listeners = new ArrayList<>();
+            String value = listenerProperty.getChild("value").getValue();
 
+            if (value != null && !value.isEmpty()) listeners.add(value);
+            if (hasTestNG) listeners.add("com.epam.jira.testng.ExecutionListener");
+            if (hasJunit) listeners.add("com.epam.jira.junit.ExecutionListener");
+
+            listenerProperty.getChild("value").setValue(String.join(",", listeners));
+
+            getLog().info("New listener property: " + listenerProperty.getValue());
         }
         return configuration;
     }
+
+
 }
